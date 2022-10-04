6dc6110e6d9915855e8fc0e8c38412e595d63668
Tianlang <tlqin0122@gmail.com>
2022-10-04 09:10:58 -0400

    20221004 backup

--- IntAndDouble.java
+++ IntAndDouble.java
@@ -1,13 +1,29 @@
+/*
+ * Tianlang Qin
+ * hw009 hw010
+ * worked with no one 
+ * advised by Jinyu Zheng
+ */
+
 public class IntAndDouble {
     public static void main(String[] args) {
-        
+        System.out.println(fibonacci(5));
+        System.out.println(fibonacci(13));
     }
 
     static int sum(int n) {
         return n * (n + 1) / 2; 
     }
 
-    static double fibonacci(double index) {
-        return (Math.pow((1 + Math.sqrt(5)), index) - Math.pow((1 - Math.sqrt(5)), index))/ (Math.pow(2, index) * Math.sqrt(5));
+    static int fibonacci(double index) {
+        /*
+            () parentheses
+            Math.pow member access
+            (int) cast
+            * multiplicative
+            + - additive/subtractive
+        */
+        double sqrt_5 = Math.sqrt(5);
+        return (int) ((Math.pow(1 + sqrt_5, index) - Math.pow(1 - sqrt_5, index)) / (Math.pow(2, index) * sqrt_5));
     }
 }
