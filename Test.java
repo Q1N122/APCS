c2a084fd06b3071292fc6ab2326c784c4be15e9a
Tianlang Qin <tlqin@shiwuhoemacbook.78m475.nycboe.org>
2022-10-14 11:18:17 -0400

    testing 1test

--- Test.java
+++ Test.java
@@ -1,7 +1,17 @@
 public class Test {
     public static void main(String[] args) {
         
-        System.out.println((int)"5");
-        System.out.println((String)(5));
+        System.out.println((int) 2.7 == 3);
+        System.out.println((int) 2.7 + 0.5);
+    }
+
+    private static int dp(int displacement, int file, int rank) {
+        int result = file * 6 + rank + displacement;
+        if (result < 0 || result > 29) {
+            return -1; 
+        }
+        else {
+            return result;
+        }
     }
 }
