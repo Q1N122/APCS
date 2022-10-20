20c2d8f252f6a59d82a307a8a22d79fb211995a2
Tianlang <tlqin0122@gmail.com>
2022-10-19 23:18:53 -0400

    20221019hw

---
+++ DateForApcs.java
@@ -0,0 +1,38 @@
+/* 
+Tianlang Qin
+hw016
+worked with no one
+helped by no one
+
+Uses for dates: 
+1. to log activities    
+2. how much time has passed since last login
+3. to decide whether something is early, late, or on time
+4. to notify a event 
+5. compare two dates
+
+class DateForApcs
+    int year 
+    int month 
+    int day 
+
+    //return the date
+    int getDate (int year, month, day)
+
+    //test whether date1 is after date 2
+    boolean after (int year1, int month1, int day1, int year2, int month2, int day2)
+    
+    //test whether date1 is before date 2
+    boolean before(int year1, int month1, int day1, int year2, int month2, int day2)  
+
+    //returns the year
+    int getYear()
+
+    //returns the month
+    int getMonth()
+
+    //returns the day
+    int getDay()
+
+change this to a block comment
+*/
